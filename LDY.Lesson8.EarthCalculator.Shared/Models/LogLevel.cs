5009e58354ed36d4dc8ec79e1bb9bd1e0af5cded
Okalash <kalachnikovsasha@gmail.com>
2018-12-08 13:12:54 +0200

    Added LogModels

---
+++ LDY.Lesson8.EarthCalculator.Shared/Models/LogLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace LDY.Lesson8.EarthCalculator.Shared.Models {
+    public enum LogLevel {
+        Info=10,
+        Debug=20,
+        Warning=30,
+        Error=40,
+        Fatal=50
+    }
+}
---
+++ LDY.Lesson8.EarthCalculator.Shared/Models/LogRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDY.Lesson8.EarthCalculator.Shared.Models {
+    public class LogRecord {
+        public LogLevel logLevel { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Message { get; set; }
+    }
+}
