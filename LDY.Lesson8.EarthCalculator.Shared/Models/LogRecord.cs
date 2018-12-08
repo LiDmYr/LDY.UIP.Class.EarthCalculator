e6d0fc950554098fd0989e1cc90cb5b435a5990c
LDY <liashkod@gmail.com>
2018-12-08 08:26:51 +0200

    Updated Logger

---
+++ LDY.Lesson8.EarthCalculator.Shared/Models/LogRecord.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDY.Lesson8.EarthCalculator.Shared.Models {
+
+    public class LogRecord {
+        [JsonIgnore]
+        public string NotSerializableField { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Message { get; set; }
+
+        public LogLevel LogLevel { get; set; }
+
+        public override string ToString() {
+            return $"___Message = {Message}; CreatedAt = {CreatedAt}; LogLevel = {LogLevel};";
+        }
+    }
+
+    public enum LogLevel {
+        Debug = 10,
+        Info = 20,
+        Warn = 30,
+        Error = 40,
+        Fatal = 50
+    }
+}
