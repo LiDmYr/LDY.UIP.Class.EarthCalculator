565c398aba60deb96b2344acc1fad20877b22c35
LDY <liashkod@gmail.com>
2018-12-08 08:25:12 +0200

    Added JSON Serializer

---
+++ LDY.Lesson8.EarthCalculator.Shared/Services/JSONSerializer.cs
@@ -0,0 +1,19 @@
+﻿using LDY.Lesson8.EarthCalculator.Shared.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDY.Lesson8.EarthCalculator.Shared.Services {
+    public class JSONSerializer : IJSONSerializer {
+        public T Deserialize<T>(string text) {
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+
+        public string Serialize<T>(T obj) {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
